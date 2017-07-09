1c90f7848ccf3f396405e172467bc22dfe372606
LucidityStill <luciditystill@tripsit.me>
2017-07-09 14:46:02 -0300

    Added fragments for the different tabs

--- app/src/main/java/me/tripsit/tripmobile/MainActivity.java
+++ app/src/main/java/me/tripsit/tripmobile/MainActivity.java
@@ -3,9 +3,15 @@ package me.tripsit.tripmobile;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import me.tripsit.tripmobile.fragments.AboutFragment;
+import me.tripsit.tripmobile.fragments.ChatFragment;
+import me.tripsit.tripmobile.fragments.ContactFragment;
+import me.tripsit.tripmobile.fragments.InfoFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,18 +22,19 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            FragmentManager manager = getSupportFragmentManager();
             switch (item.getItemId()) {
                 case R.id.navigation_chat:
-                    mTextMessage.setText(R.string.title_chat);
+                    manager.beginTransaction().replace(R.id.content, new ChatFragment()).commit();
                     return true;
                 case R.id.navigation_info:
-                    mTextMessage.setText(R.string.title_info);
+                    manager.beginTransaction().replace(R.id.content, new InfoFragment()).commit();
                     return true;
                 case R.id.navigation_about:
-                    mTextMessage.setText(R.string.title_about);
+                    manager.beginTransaction().replace(R.id.content, new AboutFragment()).commit();
                     return true;
                 case R.id.navigation_contact:
-                    mTextMessage.setText(R.string.title_contact);
+                    manager.beginTransaction().replace(R.id.content, new ContactFragment()).commit();
                     return true;
             }
             return false;
@@ -40,13 +47,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        //mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         navigation.setItemIconTintList(null);
 
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setIcon(R.drawable.ic_logo);
+
+        if (savedInstanceState != null) {
+            return;
+        }
+
+        // Create a new Fragment to be placed in the activity layout
+        ChatFragment firstFragment = new ChatFragment();
+
+        // In case this activity was started with special instructions from an
+        // Intent, pass the Intent's extras to the fragment as arguments
+        firstFragment.setArguments(getIntent().getExtras());
+
+        // Add the fragment to the 'fragment_container' FrameLayout
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.content, firstFragment).commit();
     }
 
 }
