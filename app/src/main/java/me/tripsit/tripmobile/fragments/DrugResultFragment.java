3cca19ebfbff849f79d45cb990445ff81b6a4ef2
LucidityStill <luciditystill@tripsit.me>
2017-07-09 16:15:28 -0300

    Started drug info tab

---
+++ app/src/main/java/me/tripsit/tripmobile/fragments/DrugResultFragment.java
@@ -0,0 +1,17 @@
+package me.tripsit.tripmobile.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import me.tripsit.tripmobile.R;
+
+public class DrugResultFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_drug_result, container, false);
+    }
+}
--- app/src/main/java/me/tripsit/tripmobile/fragments/InfoFragment.java
+++ app/src/main/java/me/tripsit/tripmobile/fragments/InfoFragment.java
@@ -4,14 +4,61 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import me.tripsit.tripmobile.R;
 
-public class InfoFragment extends Fragment {
+public class InfoFragment extends Fragment implements OnClickListener{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_info, container, false);
+        View v = inflater.inflate(R.layout.fragment_info, container, false);
+
+        Button b = (Button) v.findViewById(R.id.drugSearchButton);
+        b.setOnClickListener(this);
+
+        return v;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.drugSearchButton:
+                final EditText textBox = (EditText) getView().findViewById(R.id.drugTextBox);
+                final TextView resBox = (TextView) getView().findViewById(R.id.drugSearchTitle);
+
+                // Instantiate the RequestQueue.
+                RequestQueue queue = Volley.newRequestQueue(getActivity().getApplicationContext());
+                String url = "http://tripbot.tripsit.me/api/tripsit/getDrug?name=" + textBox.getText();
+
+                // Request a string response from the provided URL.
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                // Display the first 500 characters of the response string.
+                                resBox.setText("Response is: " + response);
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        resBox.setText("That didn't work!");
+                    }
+                });
+                // Add the request to the RequestQueue.
+                queue.add(stringRequest);
+                break;
+        }
     }
 }
