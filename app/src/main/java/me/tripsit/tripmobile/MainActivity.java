70bbdb0d198eead0c95e6757a6faa8c5f07bd1df
LucidityStill <luciditystill@tripsit.me>
2017-07-09 12:00:18 -0300

    Added TripSit logo

--- app/src/main/java/me/tripsit/tripmobile/MainActivity.java
+++ app/src/main/java/me/tripsit/tripmobile/MainActivity.java
@@ -44,6 +44,9 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         navigation.setItemIconTintList(null);
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.ic_logo);
     }
 
 }
